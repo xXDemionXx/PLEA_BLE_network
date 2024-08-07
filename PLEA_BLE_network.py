4974e82f6aab882798ddc8cfb25fe8559a31cee6
Demion <demion.savrljuga.business@gmail.com>
2024-08-07 16:50:08 +0200

    Connect to network added

--- PLEA_BLE_network.py
+++ PLEA_BLE_network.py
@@ -18,10 +18,15 @@ class NetworkNotificationDelegate(btle.DefaultDelegate):
         super().__init__()
         self.network_connect_ch = network_connect_ch
         self.network_commands_ch = network_commands_ch
+        self.connect_network_string = ""
+        self.connect_network_string_finished = False
 
     def handleNotification(self, cHandle, data):
         if cHandle == self.network_connect_ch.getHandle():
             print("Notification from network_connect_ch:", data)
+            self.connect_network_string += data.decode('utf-8')
+            if self.connect_network_string[-1] == '#':
+                self.connect_network_string_finished = True
         elif cHandle == self.network_commands_ch.getHandle():
             print("Notification from network_commands_ch:", data)
             handle_network_commands(data.decode('utf-8'))
@@ -29,7 +34,7 @@ class NetworkNotificationDelegate(btle.DefaultDelegate):
 def notification_loop(peripheral, stop_event):
     while not stop_event.is_set():
         try:
-            if peripheral.waitForNotifications(1.0):    # Constantly listen for notifications
+            if peripheral.waitForNotifications(1.0):  # Constantly listen for notifications
                 continue
         except btle.BTLEDisconnectError:
             print("Notification loop detected disconnection.")
@@ -46,7 +51,7 @@ def connect_to_device():
         print(f"Failed to connect: {e}")
         return None
 
-def connect_to_network_characteristics(peripheral):
+def network_service(peripheral):
     # Connect to the service
     network_service = peripheral.getServiceByUUID(UUID(NETWORK_SERVICE_UUID))
     
@@ -59,7 +64,8 @@ def connect_to_network_characteristics(peripheral):
     network_connect_ch = network_service.getCharacteristics(NETWORK_CONNECT_CH_UUID)[0]
     network_commands_ch = network_service.getCharacteristics(NETWORK_COMMANDS_CH_UUID)[0]
     
-    peripheral.setDelegate(NetworkNotificationDelegate(network_connect_ch, network_commands_ch))
+    delegate = NetworkNotificationDelegate(network_connect_ch, network_commands_ch)
+    peripheral.setDelegate(delegate)
     
     network_connect_ch_handle = network_connect_ch.getHandle() + 1
     peripheral.writeCharacteristic(network_connect_ch_handle, b'\x01\x00', withResponse=True)  # Enable notifications
@@ -67,7 +73,7 @@ def connect_to_network_characteristics(peripheral):
     network_commands_ch_handle = network_commands_ch.getHandle() + 1
     peripheral.writeCharacteristic(network_commands_ch_handle, b'\x01\x00', withResponse=True)  # Enable notifications
 
-    return network_names_ch, network_connect_ch, network_message_ch, network_commands_ch
+    return network_names_ch, network_connect_ch, network_message_ch, network_commands_ch, delegate
 
 def BLE_send_networks_string(networks_string):
     string_length = len(networks_string)
@@ -80,11 +86,10 @@ def BLE_send_networks_string(networks_string):
     if string_length % 20 != 0:  # If there is a leftover smaller than 20 chars
         chunks_array.append(networks_string[-(string_length % 20):])
     
-    for chunk in chunks_array:	# Send the chunks over BLE
+    for chunk in chunks_array:  # Send the chunks over BLE
         print(chunk)
         network_names_ch.write(bytes(str(chunk), 'utf-8'))
         time.sleep(0.003)
-
 ###
 
 # Network names #
@@ -115,7 +120,7 @@ def get_networks_string():
             device_name = parts[0].strip()
             if is_ethernet_connected(device_name):
                 networks += f"E:<<{device_name}>>"
-    networks += '#'     # Tells the receiver the string is done
+    networks += '#'  # Tells the receiver the string is done
     return networks
 ###
 
@@ -127,76 +132,116 @@ def get_ip_for_device(device):
     return ip_address if ip_address else "No IP assigned"
 ###
 
-
+# Network commands #
 def handle_network_commands(network_command):
-    
-    #print(network_command)
-    
     match network_command:
-        case 's':   # Search for networks
+        case 's':  # Search for networks
             print("Search for networks")
             networks_string = get_networks_string()
             print(networks_string)
             BLE_send_networks_string(networks_string)
             return
-        case 'p':   # Get IP
+        case 'p':  # Get IP
             print("Get IP")
             return
-        case 'd':   # Disconnect from network
+        case 'd':  # Disconnect from network
             print("Disconnect from network")
             return
         case _:
             print("Unknown command")
             return
+###
 
+# Network connect-disconnect #
+def connect_to_network(network_info):
+    # Incoming string looks like this:
+    # If it's WiFi: <<W:>><<NetworkName>><<Password>>
+    # If it's Eth:  <<E:>><<NetworkName>>
+    #
+    # Function takes in a string, chops it into type,
+    # name and password and connects to it
+    #
+    network_info = network_info[:-3]  # Removes >>#
+    network_info = network_info[2:]  # Removes <<
+    network_info_list = network_info.split('>><<')  # Makes it a list
+    
+    if len(network_info_list) < 2:  # Validate the list length
+        print("Error: network_info string is not in the expected format.")
+        return
+    
+    network_type = network_info_list[0]
+    network_name = network_info_list[1]
+    
+    if network_type == "W:":
+        network_password = network_info_list[2]
+        # Connect to WiFi
+        result = subprocess.run(['nmcli', 'device', 'wifi', 'connect', network_name, 'password', network_password], capture_output=True, text=True)
+        if result.returncode == 0:
+            print(f"Successfully connected to Wi-Fi network {network_name}")
+            # get_ip_addresses()
+        else:
+            print(f"Failed to connect to Wi-Fi network {network_name}")
+            print(result.stderr)
+    elif network_type == "E:":
+        # Connect to Ethernet
+        result = subprocess.run(['nmcli', 'device', 'connect', network_name], capture_output=True, text=True)
+        if result.returncode == 0:
+            print(f"Successfully connected to Ethernet network {network_name}")
+        else:
+            print(f"Failed to connect to Ethernet network {network_name}")
+            print(result.stderr)
+    else:
+        print("Error: Unknown network type.")
+###
 
 def BLE_main():
     # Variables #
     peripheral = None
-    new_network_command_flag = False
-    #network_command = None
+    global network_names_ch, network_connect_ch, network_message_ch, network_commands_ch, delegate
+    
     ###
-
+    
     # Declare wanted characteristics #
-    # Declare network characteristics:
-    global network_names_ch, network_connect_ch, network_message_ch, network_commands_ch
-    ###
-    notification_thread = None      # Thread for handling incoming notifications
+    notification_thread = None  # Thread for handling incoming notifications
     stop_event = threading.Event()
 
     while True:
         if peripheral is None:  # We are not connected
-            peripheral = connect_to_device()    # Try to connect
-            if peripheral is not None:          # If we succeeded
+            peripheral = connect_to_device()  # Try to connect
+            if peripheral is not None:  # If we succeeded
                 # Connect to wanted characteristics #
-                # Connect to network characteristics
-                network_names_ch, network_connect_ch, network_message_ch, network_commands_ch = connect_to_network_characteristics(peripheral)
+                network_names_ch, network_connect_ch, network_message_ch, network_commands_ch, delegate = network_service(peripheral)
                 ###
                 stop_event.clear()
                 notification_thread = threading.Thread(target=notification_loop, args=(peripheral, stop_event))
                 notification_thread.daemon = True
                 notification_thread.start()
-            else:   # If we didn't succeed, retry
+            else:  # If we didn't succeed, retry
                 print("Retrying in 5 seconds...")
                 time.sleep(5)
 
-        else:   # We are connected
-            if stop_event.is_set(): # If we lost the connection
+        else:  # We are connected
+            if stop_event.is_set():  # If we lost the connection
                 print("Connection lost, attempting to reconnect...")
                 if notification_thread:
                     notification_thread.join()
                 try:
-                    peripheral.disconnect() # Ensure disconnection
+                    peripheral.disconnect()  # Ensure disconnection
                 except Exception as e:
                     print(f"Error during disconnection: {e}")
                 peripheral = None
                 time.sleep(5)
             else:
-                try:    # Main loop handling
-                    if new_network_command_flag == True:
-                        print("Received command: ")
-                        #handle_network_commands(network_command)
-                        #new_network_command_flag = False
+                try:  # Main loop handling
+                    if delegate.connect_network_string_finished:
+                        print("End of connect_network_string")
+                        print(delegate.connect_network_string)
+                        
+                        connect_to_network(delegate.connect_network_string)
+                        
+                        # After connecting reset flag and string
+                        delegate.connect_network_string = ""
+                        delegate.connect_network_string_finished = False
                 except btle.BTLEDisconnectError:
                     print("Detected disconnection during main loop tasks.")
                     stop_event.set()
